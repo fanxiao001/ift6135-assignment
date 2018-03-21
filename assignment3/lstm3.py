fix calculation of cost

assignment3/lstm3.py
@@ -120,7 +120,7 @@ def train(model, train_data_loader, criterion, optimizer,  interval = 1000, disp
             y_out[i], hidden = model(Variable(torch.zeros(1,batch_size, output_size+1)),hidden)
         
 #        length = outp_seq_len * batch_size
-        length = outp_seq_len * batch_size
+        length = batch_size
         lengthes +=  length
     
 #            y_out = tag_scores[:outp_seq_len,:] #remove end of sequence indicator
@@ -137,13 +137,18 @@ def train(model, train_data_loader, criterion, optimizer,  interval = 1000, disp
         loss.backward()
         optimizer.step()
         
-        if batch_num % interval == 0 or batch_num == 1 :
+        if batch_num % interval == 0  :
             list_loss.append(losses.data/lengthes)
             list_seq_num.append(lengthes/1000) # per thousand
             list_cost.append(costs/lengthes)
+
         
-        if display and (batch_num % interval == 0  or batch_num == 1): 
+        if display and (batch_num % interval == 0 ): 
             print ("Epoch %d, loss %f, cost %f" % (batch_num, losses/lengthes, costs/lengthes) )
+            costs = 0
+            losses = 0
+            
+            
     return list_seq_num, list_loss, list_cost
 
 def evaluate(model, test_data_loader, criterion) : 
@@ -215,8 +220,8 @@ MIN_LENGTH = 1
 MAX_LENGTH = 20
 LEARNING_RATE = 3e-5
 MOMENTUM = 0.9 
-MINI_BATCH = 1
-EPOCH = 1000000
+MINI_BATCH = 100
+EPOCH = 10000
 
 model = LSTMCopy(NUM_BITS+1,LSTM_DIM,NUM_BITS,1,MINI_BATCH)
 
@@ -225,12 +230,12 @@ optimizer = optim.RMSprop(model.parameters(), lr=LEARNING_RATE, momentum = MOMEN
 
 train_data_loader = dataloader(EPOCH,MINI_BATCH,NUM_BITS,MIN_LENGTH,MAX_LENGTH)
 
-list_seq_num, list_loss, list_cost = train(model, train_data_loader, loss_function, optimizer, interval=1000)
+list_seq_num, list_loss, list_cost = train(model, train_data_loader, loss_function, optimizer, interval=100)
 
 #%%
 plt.plot(list_seq_num,list_cost)
 #%%
-saveCheckpoint(model,list_seq_num,list_loss, list_cost, path='lstm3_l1_b20_e10000') 
+saveCheckpoint(model,list_seq_num,list_loss, list_cost, path='lstm3_l1_b100_e10000') 
 
 #%%
 model, list_seq_num, list_loss, list_cost = loadCheckpoint(path='lstm3')